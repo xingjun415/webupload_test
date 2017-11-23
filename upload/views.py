ff3f32c29c37200923f3947f3fa26e88abbfcbb9
xinjun <smu_lxj@163.com>
2017-11-23 09:44:58 +0800

    Add bootstrap in html

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 
 def index(request):
@@ -9,10 +8,14 @@ def index(request):
 
 def data(request):
     print("OK")
-    files = request.FILES['file']
-    print(type(files))
-    print(dir(files))
-    with open("./" + files.name, "wb") as file:
-        for file_upload in files:
-            file.write(file_upload)
+    uploadfile = request.FILES['file']
+    print(type(uploadfile))
+    print(dir(uploadfile))
+    chunk_size = 10 * 2 ** 20
+    written_size = 0
+    with open("./" + uploadfile.name, "wb") as file:
+        for chunk in uploadfile.chunks(chunk_size):
+            file.write(chunk)
+            written_size += len(chunk)
+            print("Percentage : ", str(written_size / uploadfile.size * 100 ) + "%")
     return HttpResponse("OK")
